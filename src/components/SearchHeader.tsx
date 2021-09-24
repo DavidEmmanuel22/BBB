409bedd69588c4a9be6a9f3a8e22450a8f2cd694
jeenDzul <jeen.dzul@meru.com>
2021-09-23 23:18:18 -0500

    fix/order columns

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from 'react';
 import { View, TextInput, TouchableOpacity, Image } from 'react-native';
 import { StackHeaderProps } from '@react-navigation/stack';
-import { BLUE } from '../constants/colors';
+import { BLUE, LIGHTER_GRAY2 } from '../constants/colors';
 import { changeSelect, selectUIISelected } from '../store/slices/uiSlice';
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -42,23 +42,27 @@ const SearchHeader: React.FC<IProps> = ({ close = false, ...props }) => {
 
   return (
 
-    <View style={{ width:"100%", height: 90, backgroundColor: BLUE, justifyContent:'center', alignItems:'center'}}>
+    <View style={{ width: "100%", height: 90, backgroundColor: BLUE, justifyContent: 'center', alignItems: 'center' }}>
 
-      <View style={{flexDirection:'row'}}>
-        <View style={{ width:"75%"}}>
-          <TextInput placeholderTextColor='gray' placeholder='Buscar' style={{borderRadius:200, backgroundColor:'white', color:'black', paddingLeft:36, fontSize:16, height:40}}></TextInput>
+      <View style={{ flexDirection: 'row' }}>
+        <View style={{ width: "75%" }}>
+          <TextInput placeholder="Busca tu producto"
+            placeholderTextColor={LIGHTER_GRAY2}
+            onFocus={handleFocus}
+            ref={searchInputRef}
+            style={{ borderRadius: 200, backgroundColor: 'white', color: 'black', paddingLeft: 36, fontSize: 16, height: 40 }}></TextInput>
           <TouchableOpacity style={{ position: 'absolute', right: 1, marginTop: 12, marginRight: 20 }}>
             <Image source={require('../assets/QRIcon.png')}></Image>
           </TouchableOpacity>
 
           <Image style={{ position: 'absolute', left: 1, marginTop: 10, marginLeft: 12 }} source={require('../assets/SearchIcon.png')}></Image>
         </View>
-        <TouchableOpacity style={{ alignSelf:'center', marginLeft:20}}>
-            <Image source={require('../assets/CarIcon.png')}></Image>
-          </TouchableOpacity>
+        <TouchableOpacity style={{ alignSelf: 'center', marginLeft: 20 }}>
+          <Image source={require('../assets/CarIcon.png')}></Image>
+        </TouchableOpacity>
 
       </View>
-      
+
     </View>
 
   );
