5d2e8392d5ddd974bc626417f16883b9088d2bcd
jeenDzul <jeen.dzul@meru.com>
2021-09-23 23:22:57 -0500

    fix/order columns

--- App.tsx
+++ App.tsx
@@ -8,18 +8,13 @@ import Splash from './src/screens/Splash';
 
 import {store} from './src/store';
 
-//import {ThemeProvider} from 'react-native-magnus';
-//import theme from './src/theme/theme';
-
 const App = () => {
   return (
     <Provider store={store}>
-      {/*<ThemeProvider>*/}
       <NavigationContainer>
         <Splash />
         <BottomTabNavigation />
       </NavigationContainer>
-      {/* </ThemeProvider>*/}
     </Provider>
   );
 };
