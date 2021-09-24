77fcadff6063da47e0d70d9d22105eb391fde2b0
Ruben Manuel Elizalde Salazar <91155791+RubenSalazar-dgk@users.noreply.github.com>
2021-09-23 22:13:31 -0500

    Search bar change

--- src/components/SearchHeader.tsx
+++ src/components/SearchHeader.tsx
@@ -1,15 +1,9 @@
-import React, {useRef, useEffect, useState} from 'react';
-import {View, TextInput, StyleSheet, FlatList} from 'react-native';
-import {StackHeaderProps} from '@react-navigation/stack';
-
-import Container, {StatusBarStyle} from './Container';
-import Icon from './Icon';
-import {DARK, BLUE, WHITE, LIGHTER_GRAY2} from '../constants/colors';
-import {EFFRA} from '../constants/fonts';
-import {getHeight, getWidth} from '../utils/interfaceDimentions';
-import CategoryItem from '../components/core/home/CategoryItem';
-import {changeSelect, selectUIISelected} from '../store/slices/uiSlice';
-import {useDispatch, useSelector} from 'react-redux';
+import React, { useRef, useEffect, useState } from 'react';
+import { View, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StackHeaderProps } from '@react-navigation/stack';
+import { BLUE } from '../constants/colors';
+import { changeSelect, selectUIISelected } from '../store/slices/uiSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 interface itemNav {
   id: Number;
@@ -17,16 +11,16 @@ interface itemNav {
   nameNav: string;
 }
 const categories: Array<itemNav> = [
-  {id: 0, text: 'Destacados', nameNav: 'Index'},
-  {id: 1, text: 'Categorías', nameNav: 'Categories'},
-  {id: 2, text: 'Zona oulet', nameNav: ''},
-  {id: 4, text: 'Mesa', nameNav: ''},
+  { id: 0, text: 'Destacados', nameNav: 'Index' },
+  { id: 1, text: 'Categorías', nameNav: 'Categories' },
+  { id: 2, text: 'Zona oulet', nameNav: '' },
+  { id: 4, text: 'Mesa', nameNav: '' },
 ];
 interface IProps extends StackHeaderProps {
   close?: boolean;
 }
 
-const SearchHeader: React.FC<IProps> = ({close = false, ...props}) => {
+const SearchHeader: React.FC<IProps> = ({ close = false, ...props }) => {
   const dispatch = useDispatch();
   const categorySelect = useSelector(selectUIISelected);
   const searchInputRef = useRef<any>(null);
@@ -45,117 +39,30 @@ const SearchHeader: React.FC<IProps> = ({close = false, ...props}) => {
     props.navigation.navigate(item.nameNav);
     dispatch(changeSelect(item.text));
   };
+
   return (
-    <>
-      <View style={styles.container}>
-        <Container
-          scroll={false}
-          contentWrapperStyles={styles.wrapper}
-          statusBarStyle={StatusBarStyle.LIGHT}>
-          <View style={styles.formGroup}>
-            <Icon
-              name="search-icon"
-              size={16}
-              color={LIGHTER_GRAY2}
-              style={[styles.icon, styles.searchIcon]}
-            />
-            <TextInput
-              placeholder="Busca tu producto"
-              placeholderTextColor={LIGHTER_GRAY2}
-              onFocus={handleFocus}
-              ref={searchInputRef}
-              style={styles.formControl}
-            />
-            <Icon
-              name="barcode-icon"
-              size={16}
-              color={DARK}
-              onPress={() => {}}
-              style={[styles.icon, styles.barcodeIcon]}
-            />
-          </View>
-          <View style={styles.headerRight}>
-            <Icon name="cart-icon" size={24} color={WHITE} onPress={() => {}} />
-            {close && (
-              <Icon
-                name="cross-icon"
-                size={22}
-                color={WHITE}
-                style={styles.crossIcon}
-                onPress={() => {}}
-              />
-            )}
-          </View>
-        </Container>
+
+    <View style={{ width:"100%", height: 90, backgroundColor: BLUE, justifyContent:'center', alignItems:'center'}}>
+
+      <View style={{flexDirection:'row'}}>
+        <View style={{ width:"75%"}}>
+          <TextInput placeholderTextColor='gray' placeholder='Buscar' style={{borderRadius:200, backgroundColor:'white', color:'black', paddingLeft:36, fontSize:16, height:40}}></TextInput>
+          <TouchableOpacity style={{ position: 'absolute', right: 1, marginTop: 12, marginRight: 20 }}>
+            <Image source={require('../assets/QRIcon.png')}></Image>
+          </TouchableOpacity>
+
+          <Image style={{ position: 'absolute', left: 1, marginTop: 10, marginLeft: 12 }} source={require('../assets/SearchIcon.png')}></Image>
+        </View>
+        <TouchableOpacity style={{ alignSelf:'center', marginLeft:20}}>
+            <Image source={require('../assets/CarIcon.png')}></Image>
+          </TouchableOpacity>
+
       </View>
-      <View style={{flexDirection: 'row'}}>
-        <FlatList
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          data={categories}
-          renderItem={({item}: any) => (
-            <View key={item.id} style={{marginRight: 28}}>
-              <CategoryItem
-                isSelected={item.text === categorySelect}
-                label={item.text}
-                onPress={() => {
-                  handleNav(item);
-                }}
-              />
-            </View>
-          )}
-          keyExtractor={item => 'key' + item.id}
-        />
-      </View>
-    </>
+      
+    </View>
+
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: BLUE,
-    height: getHeight(100),
-  },
-  wrapper: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  formGroup: {
-    flex: 1,
-    position: 'relative',
-    marginRight: getWidth(20),
-  },
-  formControl: {
-    backgroundColor: WHITE,
-    fontFamily: EFFRA,
-    fontSize: getWidth(16),
-    height: getWidth(40),
-    paddingLeft: getWidth(36),
-    paddingRight: getWidth(36),
-    borderRadius: getWidth(15),
-    width: '100%',
-  },
-  icon: {
-    position: 'absolute',
-    top: getWidth(12),
-    zIndex: 1,
-  },
-  searchIcon: {
-    left: getWidth(14),
-  },
-  barcodeIcon: {
-    right: getWidth(16),
-  },
-  headerRight: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-
-  crossIcon: {
-    marginLeft: getHeight(10),
-  },
-});
 
 export default SearchHeader;
